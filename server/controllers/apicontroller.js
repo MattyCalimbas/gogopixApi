6c5433ae6a34c7123dbf943219357a22f645f2f2
Matthew Calimbas <45830182+MattyCalimbas@users.noreply.github.com>
2019-12-12 10:03:57 -0800

    api controller updated
    added two new fields: link_out and save_avail

--- server/controllers/apicontroller.js
+++ server/controllers/apicontroller.js
@@ -20,7 +20,7 @@ var getSubjects = (request, response) => {
 
 // modified query below (see: s."sub_Name") 6/21
 var getWallpapers = (request, response) => {
-  pool.query('SELECT w.nid, w.s_name, w.s_thumbnail_url, w.s_wallpaper_url, w.s_thumb_s3_key, w.s_wallp_s3_key, w.b_is_premium, w.b_is_active, w.source, s."sub_Name", s."ID" FROM public.wallpaper w LEFT JOIN public.wallpaper_subject_map m ON w.nid = m.wallpaper_id LEFT JOIN public.subject s ON m.subject_id = s."ID";', (error, results) => {
+  pool.query('SELECT w.nid, w.s_name, w.s_thumbnail_url, w.s_wallpaper_url, w.s_thumb_s3_key, w.s_wallp_s3_key, w.b_is_premium, w.b_is_active, w.source, w.link_out, w.save_avail, s."sub_Name", s."ID" FROM public.wallpaper w LEFT JOIN public.wallpaper_subject_map m ON w.nid = m.wallpaper_id LEFT JOIN public.subject s ON m.subject_id = s."ID";', (error, results) => {
     if (error) {
       throw error
     }
